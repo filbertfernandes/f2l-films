8b4fcbc03c3a4bddc17abdd16c87bae082b2663b
filbertfernandes <filbertfernandes.bot@gmail.com>
2021-05-10 15:54:07 +0700

    modified

--- js/script.js
+++ js/script.js
@@ -74,7 +74,7 @@ $('.click-here').on('click', function() {
 						  <div class="col-lg-10 col-12 mb-3 fade-in">
 						    <div class="card card-6">
 						      <div class="card-body">
-						        <h2 class="font-weight-bold short-review-h2">"${filmId[1]}"</h2>
+						        <h2 class="font-weight-bold short-review-h2">${filmId[1]}</h2>
 						        <h5 class="short-review-h5">My Short Review</h5>
 						      </div>
 						    </div>
@@ -87,22 +87,39 @@ $('.click-here').on('click', function() {
 })
 
 function randomFilm() {
-	let random = Math.floor(Math.random() * 6);
+	let random = Math.floor(Math.random() * 29);
 	let film = [];
 
-	if( random === 0 ) film = ['tt9000302', 'One of the best Indonesian psychological horror movie.'];
-	if( random === 1 ) film = ['tt5052448', 'I will always love great thriller-mystery film, and this is one of them.'];
-	if( random === 2 ) film = ['tt1727824', 'As a huge fan of the band Queen, I watched this film on theater 3 times.'];
-	if( random === 3 ) film = ['tt6751668', 'The best work from Bong Joon-Ho, very well-deserved Oscar Best Picture.'];
-	if( random === 4 ) film = ['tt7286456', 'Joaquin Phoenix totally deserved an oscar for his role as Joker.'];
-	if( random === 5 ) film = ['tt8579674', 'One continuous shot with a very beautiful cinematography, 1917 is definitely one of my favorite world war film.'];
+	if( random === 0 ) film = ['tt9000302', '"One of the best Indonesian psychological horror movie."'];
+	if( random === 1 ) film = ['tt5052448', '"I will always love great thriller-mystery film, and this is one of them."'];
+	if( random === 2 ) film = ['tt1727824', '"As a huge fan of the band Queen, I watched this film on theater 3 times."'];
+	if( random === 3 ) film = ['tt6751668', '"The best work from Bong Joon-Ho, very well-deserved Oscar Best Picture."'];
+	if( random === 4 ) film = ['tt7286456', '"Joaquin Phoenix totally deserved an oscar for his role as Joker."'];
+	if( random === 5 ) film = ['tt8579674', '"One continuous shot with a beautiful cinematography, 1917 is definitely one of my favorite world war film."'];
+	if( random === 6 ) film = ['tt5753856', '"DARK is the GREATEST series on Netflix. Period."'];
+	if( random === 7 ) film = ['tt1899353', '"No other action films better than The Raid duology, so far."'];
+	if( random === 8 ) film = ['tt2265171', '"No other action films better than The Raid duology, so far."'];
+	if( random === 9 ) film = ['tt4911656', '"Realistic, romantic, and terrific. My most favorite work from Joko Anwar."'];
+	if( random === 10 ) film = ['tt1392214', '"One of the best thriller-mystery film I have ever seen, until now."'];
+	if( random === 11 ) film = ['tt4574334', '"One of my favorite Netflix show. 80s era is the best."'];
+	if( random === 12 ) film = ['tt12361974', '"ZSJL is more than just comicbook film, it is a masterpiece of cinema."'];
+	if( random === 13 ) film = ['tt2085059', '"Watch the 1st episode of Black Mirror and you\'re gonna see how amazing this show is."'];
+	if( random === 14 ) film = ['tt0088763', '"A very entertaining time travel movie, never gets old."'];
+	if( random === 15 ) film = ['tt0120338', '"It feels like you\'re in Titanic when watching this, beautiful and terrifying."'];
+	if( random === 16 ) film = ['tt2015381', '"The film that made me love MCU even more."'];
+	if( random === 17 ) film = ['tt2119532', '"This film proved that Andrew Garfield is an amazing actor."'];
+	if( random === 18 ) film = ['tt4154756', '"The hype on this film is beyond infinity."'];
+	if( random === 19 ) film = ['tt4154796', '"Felt goosebumps, nostalgia, and emotional watching this."'];
+	if( random === 20 ) film = ['tt3896198', '"One of the best Marvel films."'];
+	if( random === 21 ) film = ['tt0110912', '"Jules will always be my favorite character in cinema history!"'];
+	if( random === 22 ) film = ['tt7221388', '"This show is badass!"'];
+	if( random === 22 ) film = ['tt0111161', '"Rewatching this whenever I feel down."'];
+	if( random === 24 ) film = ['tt1457767', '"James Wan\'s horror films will always be my favorite."'];
+	if( random === 25 ) film = ['tt6763664', '"One of the best horror show I\'ve ever seen."'];
+	if( random === 26 ) film = ['tt1190634', '"Bored of superhero \'save the world\' kind of film? This show is what you need!"'];
+	if( random === 27 ) film = ['tt0264464', '<span style="color: red;font-style: italic;">no review added yet</span>'];
+	if( random === 28 ) film = ['tt0137523', '<span style="color: red;font-style: italic;">no review added yet</span>'];
+	
 
 	return film;
 }
-
-// 0 = Impetigore (2019)
-// 1 = Get Out (2017)
-// 2 = Bohemian Rhapsody (2018)
-// 3 = Parasite (2019)
-// 4 = Joker (2019)
-// 5 = 1917 (2019)
